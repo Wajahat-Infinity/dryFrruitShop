5ebc22f3a3853a492f1f5c7077b639ce18af4ad4
Wajahat <wajahath533@gmail.com>
2024-10-07 23:27:54 +0500

    update day 4

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from . models import Product
+
+@admin.register(Product)
+class ProductModelAdmin(admin.ModelAdmin):
+    list_display=['id','title','selling_price','discount_price','description','composition','prodapp','category','product_image' ]
+
 # Register your models here.
--- app/models.py
+++ app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+CATEGORY_CHOICES=(('AP','APRICOT'),('MB','MALLBERRY'),('WN','WALLNUT'),('PT','PISTACHU'),('AL','ALMOND'),('WH','WAJAHATHUS'),)
+
+class Product(models.Model):
+    title=models.CharField(max_length=100)
+    selling_price=models.FloatField()
+    discount_price=models.FloatField()
+    description=models.TextField()
+    composition=models.TextField(default='')
+    prodapp=models.TextField(default='')
+    category=models.CharField(choices=CATEGORY_CHOICES, max_length=2)
+    product_image=models.ImageField(upload_to='product')
+
+    def __str__(self):
+        return self.title
+    
+
+
