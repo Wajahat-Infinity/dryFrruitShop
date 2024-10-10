ae3df70f8fb86d8deb65638140eec9261f8de7d0
Wajahat <wajahath533@gmail.com>
2024-10-10 22:11:22 +0500

    num-cat transform

--- app/admin.py
+++ app/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-
-from . models import Product
+from .models import Product
 
 @admin.register(Product)
 class ProductModelAdmin(admin.ModelAdmin):
-    list_display=['id','title','selling_price','discount_price','description','composition','prodapp','category','product_image' ]
+    list_display = ['id', 'title', 'selling_price', 'discount_price', 'description', 'composition', 'prodapp', 'category', 'product_image']
+    list_filter = ['category', 'prodapp']  # Add filters based on category and product application
+    search_fields = ['title', 'description']  # Enable search by title and description
+    ordering = ['id']  # Sort by ID in the list view
 
 # Register your models here.
--- app/views.py
+++ app/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from urllib import request
+from django.http import HttpResponse
+from django.views import View 
 
 # Create your views here.
 def home(request):
     return render(request,"app/home.html")
+
+class CategoryView(View):
+    def get(self,request,val):
+        context={
+            'val':val
+        }
+        return render(request,"app/category.html",context)
