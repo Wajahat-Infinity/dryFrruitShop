aeb939f985e8ae578264393f0de36197da3b24a2
Wajahat <wajahath533@gmail.com>
2024-10-25 22:48:43 +0500

    added the about page and contact page

--- app/urls.py
+++ app/urls.py
@@ -5,5 +5,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path('', views.home),
+    path('about/', views.about, name="about"),
+    path('contact/', views.contact, name="contact"),
     path('category/<slug:val>',views.CategoryView.as_view(), name="category"),
+    path('product-detail/<int:pk>',views.ProductDetailView.as_view(), name="productdetail"),
+    path('product-title/<slug:title>', views.CategoryTitleView.as_view(), name="product-title"),
    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- app/views.py
+++ app/views.py
@@ -8,6 +8,12 @@ from . models import Product
 def home(request):
     return render(request,"app/home.html")
 
+def about(request):
+    return render(request,"app/about.html")
+
+def contact(request):
+    return render(request,"app/contact.html")
+
 class CategoryView(View):
     def get(self, request, val):
         # Filter products based on the category value (e.g., 'AP' for APRICOT)
@@ -21,3 +27,21 @@ class CategoryView(View):
         
         # Render the template with the updated context
         return render(request, "app/category.html", context)
+
+class ProductDetailView(View):
+    def get(self,request,pk):
+        product=Product.objects.get(pk=pk)
+        return render(request,"app/productdetail.html",locals())    
+    
+# views.py
+class CategoryTitleView(View):
+    def get(self, request, title):  # Change 'val' to 'title' here
+        products = Product.objects.filter(title=title)
+        titles = Product.objects.filter(category=products[0].category).values('title')
+        context = {
+            'title': title,  # Pass 'title' to the context if needed
+            'products': products,
+            'titles': titles,
+        }
+        return render(request, "app/category.html", context)
+
